b332313a3a048947c5559233924ad863a2faa36c
pine3ree <pine3ree@gmail.com>
2020-04-30 03:08:14 +0200

    Select done!
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- src/Sql/Statement/Select.php
+++ src/Sql/Statement/Select.php
@@ -8,11 +8,18 @@
 
 namespace P3\Db\Sql\Statement;
 
+use InvalidArgumentException;
+use P3\Db\Sql;
 use P3\Db\Sql\Condition\Having;
 use P3\Db\Sql\Condition\On;
 use P3\Db\Sql\Condition\Where;
+use P3\Db\Sql\Literal;
+use P3\Db\Sql\Predicate;
+use P3\Db\Sql\PredicateSet;
 use P3\Db\Sql\Statement\DML;
 use P3\Db\Sql\Statement\Traits\ConditionAwareTrait;
+use PDO;
+use RuntimeException;
 
 /**
  * This class represents a SELECT sql-statement expression
@@ -25,29 +32,231 @@ class Select extends DML
 {
     use ConditionAwareTrait;
 
+    /** @var string|null */
+    protected $quantifier;
+
+    /** @var bool */
+    protected $distinct = false;
+
+    /** @var string[] */
+    protected $columns = [
+        Sql::ASTERISK => Sql::ASTERISK,
+    ];
+
     /** @var Where|null */
     protected $where;
+
+    /** @var array */
+    protected $join;
+
+    /** @var array */
+    protected $groupBy = [];
 
     /** @var Having|null */
     protected $having;
 
-    /** @var On|null */
-    protected $on;
+    /** @var array */
+    protected $orderBy = [];
 
-    public function getSQL(bool $stripConditionsParentheses = false): string
+    /** @var inti|null */
+    protected $limit;
+
+    /** @var int|null */
+    protected $offset;
+
+    public function __construct($columns = null, $table = null)
     {
-
+        if (!empty($columns)) {
+            $this->columns($columns);
+        }
+        if (!empty($table)) {
+            $this->from($table);
+        }
     }
 
-    public function getBaseSQL(bool $stripConditionsParentheses = false): string
+    /**
+     * @param string $quantifier
+     * @return $this
+     */
+    public function quantifier(string $quantifier): self
     {
+        $quantifier = strtoupper($quantifier);
+        if (in_array($quantifier, [Sql::DISTINCT, Sql::ALL], true)) {
+            $this->quantifier = $quantifier;
+        }
 
+        return $this;
     }
 
-    /** @var string|array|Predicate|Where| */
+    /**
+     * @param string $quantifier
+     * @return $this
+     */
+    public function distinct(): self
+    {
+        $this->quantifier = Sql::DISTINCT;
+        return $this;
+    }
+
+    /**
+     *
+     * @param type $columns
+     * @return $this
+     */
+    public function columns($columns): self
+    {
+        if (empty($columns)) {
+            return $this;
+        }
+
+        if (Sql::ASTERISK === $columns) {
+            $this->columns = [
+                Sql::ASTERISK => Sql::ASTERISK,
+            ];
+        }
+
+        if (!is_array($columns)) {
+            throw new InvalidArgumentException(sprintf(
+                "The SELECT columns argumen must be either the ASTERISK string"
+                . " or an array of column names, '%s' provided!",
+                gettype($columns)
+            ));
+        }
+
+        $this->columns = $columns;
+
+        unset($this->sql, $this->sqls['columns']);
+
+        return $this;
+    }
+
+    private function getColumnsSQL(): string
+    {
+        if (isset($this->sqls['columns'])) {
+            return $this->sqls['columns'];
+        }
+
+        $sqls = [];
+        foreach ($this->columns as $alias => $column) {
+            if ($column === Sql::ASTERISK ) {
+                $column_sql = $this->alias ? $this->quoteAlias($this->alias) . ".*" : "*";
+            } else {
+                $column_sql = $column instanceof Literal
+                    ? (string)$column
+                    : $this->normalizeColumn($column);
+                if (!is_numeric($alias)) {
+                    $column_sql .= " AS " . $this->quoteAlias($alias);
+                }
+            }
+            $sqls[] = $column_sql;
+        }
+
+        $sql = trim(implode(", ", $sqls));
+        $this->sqls['columns'] = $sql;
+
+        return $sql;
+    }
+
+    /**
+     * Set the SELECT FROM table
+     *
+     * @param string|array $table
+     * @param string|null $alias
+     * @return \self
+     */
+    public function from($table, string $alias = null): self
+    {
+         parent::setTable($table, $alias);
+         return $this;
+    }
+
+    public function join(string $table, string $alias, $cond = null): self
+    {
+        return $this->addJoin(Sql::JOIN_AUTO, $table, $alias, $cond);
+    }
+
+    public function innerJoin(string $table, string $alias, $cond = null): self
+    {
+        return $this->addJoin(Sql::JOIN_INNER, $table, $alias, $cond);
+    }
+
+    public function leftJoin(string $table, string $alias, $cond = null): self
+    {
+        return $this->addJoin(Sql::JOIN_LEFT, $table, $alias, $cond);
+    }
+
+    public function rightJoin(string $table, string $alias, $cond = null): self
+    {
+        return $this->addJoin(Sql::JOIN_RIGHT, $table, $alias, $cond);
+    }
+
+    /**
+     *
+     * @param string $type
+     * @param string $table
+     * @param string $alias
+     * @param On|PredicateSet|Predicate|Literal|Predicate\Literal|array|string $cond
+     *      The join conditional usually an ON clause, but may be changed using Literal classes
+     * @return $this
+     */
+    protected function addJoin(string $type, string $table, string $alias, $cond = null): self
+    {
+        if (! $cond instanceof On
+            && ! $cond instanceof Literal
+        ) {
+            $cond = new On(Sql::AND, $cond);
+        }
+
+        $this->join[] = [
+            'type'  => $type,
+            'table' => $table,
+            'alias' => $alias,
+            'cond'  => $cond,
+        ];
+
+        unset($this->sql, $this->sqls['join']);
+
+        return $this;
+    }
+
+    protected function getJoinSQL(): string
+    {
+        if (empty($this->join)) {
+            return '';
+        }
+
+        if (isset($this->sqls['join'])) {
+            return $this->sqls['join'];
+        }
+
+        $sqls = [];
+        foreach ($this->join as $join) {
+            $type  = isset(Sql::JOIN_TYPES[$join['type']]) ? $join['type'] : '';
+            $table = $this->quoteIdentifier($join['table']);
+            $alias = $this->quoteAlias($join['alias']);
+            $cond  = $join['cond']->getSQL(false);
+
+            if ($join['cond']->hasParams()) {
+                $this->importParams($join['cond']);
+            }
+
+            $sqls[] = trim("{$type} JOIN {$table} {$alias} {$cond}");
+        }
+
+        $this->sqls['join'] = $sql = trim(implode(" ", $sqls));
+        return $sql;
+    }
+
+    /**
+     * Add WHERE conditions
+     *
+     * @param string|array|Predicate|Where $where
+     * @return $this
+     */
     public function where($where): self
     {
-        return $this->setCondition('where', Where::class, $where);
+        $this->setCondition('where', Where::class, $where);
+        return $this;
     }
 
     protected function getWhereSQL(bool $stripParentheses = false): string
@@ -55,10 +264,58 @@ class Select extends DML
         return $this->getConditionSQL('where', $stripParentheses);
     }
 
-    /** @var string|array|Predicate|Having| */
+    /**
+     *
+     * @param sring|string[] $groupBy
+     * @param bool $replace
+     * @return $this
+     * @throws InvalidArgumentException
+     */
+    public function groupBy($groupBy, bool $replace = false): self
+    {
+        if ($replace) {
+            $this->groupBy = [];
+        }
+
+        if (is_array($groupBy)) {
+            foreach ($this->groupBy as $identifier) {
+                $this->groupBy($identifier);
+            }
+            return $this;
+        }
+
+        if (!is_string($groupBy)) {
+            throw new InvalidArgumentException(sprintf(
+                "The `groupBy` argument must be either a string or an array of"
+                . " string identifiers, `%s` provided",
+                gettype($groupBy)
+            ));
+        }
+
+        $this->groupBy[] = $groupBy;
+
+        return $this;
+    }
+
+    protected function getGroupBySQL(): string
+    {
+        if (empty($this->groupBy)) {
+            return '';
+        }
+
+        return "GROUP BY " . implode(", ", $this->groupBy);
+    }
+
+    /**
+     * Add WHERE conditions
+     *
+     * @param string|array|Predicate|Having $having
+     * @return $this
+     */
     public function having($having): self
     {
-        return $this->setCondition('having', Having::class, $having);
+        $this->setCondition('having', Having::class, $having);
+        return $this;
     }
 
     protected function getHavingSQL(bool $stripParentheses = false): string
@@ -66,17 +323,180 @@ class Select extends DML
         return $this->getConditionSQL('having', $stripParentheses);
     }
 
-    /** @var string|array|Predicate|Having| */
-    public function on($on): self
+    /**
+     *
+     * @param type $orderBy
+     * @param null|string|true $sortOrReplace Set the default sort or the replace flag
+     * @return \self
+     */
+    public function orderBy($orderBy, $sortOrReplace = null): self
     {
-        return $this->setCondition('on', On::class, $on);
+        if (true === $sortOrReplace) {
+            $this->orderBy = [];
+        }
+
+        $sort = is_string($sortOrReplace) ? strtoupper($sortOrReplace) : null;
+
+        $orderBy = $this->nomalizeOrderBy($orderBy, $sort);
+        if (empty($orderBy)) {
+            return $this;
+        }
+
+        $this->orderBy = $orderBy + (array)$this->orderBy;
+
+        return $this;
     }
 
-    protected function getOnSQL(bool $stripParentheses = false): string
+    private function nomalizeOrderBy($orderBy, string $sort = null): array
     {
-        return $this->getConditionSQL('on', $stripParentheses);
+        if (is_string($orderBy)) {
+            if (false === strpos($orderBy, ',')) {
+                $orderBy = [$orderBy];
+            } else {
+                $orderBy = array_map('trim', explode(',', $orderBy));
+            }
+        }
+
+        if (!is_array($orderBy)) {
+            throw new \InvalidArgumentException(
+                "The ORDER BY options must be either an array or a string!"
+            );
+        }
+
+        $normalized = [];
+
+        foreach ($orderBy as $identifier => $direction) {
+            if (is_numeric($identifier)) {
+                $identifier = $direction;
+                $direction  = $sort;
+            }
+
+
+            $parts = array_map('trim', explode(' ', $identifier));
+            $identifier = $parts[0];
+            $direction  = $parts[1] ?? $direction ?? Sql::ASC;
+            $direction  = $direction === Sql::DESC ? $direction : Sql::ASC;
+
+            $normalized[$identifier] = $direction;
+        }
+
+        return $normalized;
     }
 
+    private function getOrderBySQL(): string
+    {
+        if (empty($this->orderBy)) {
+            return '';
+        }
+
+        if (isset($this->sqls['order'])) {
+            return $this->sqls['order'];
+        }
+
+        $sql = [];
+        foreach ($this->orderBy as $identifier => $direction) {
+            // do not quote identifier or alias, do it programmatically
+            $sql[] = "{$identifier} {$direction}";
+        }
+
+        $this->sqls['order'] = $sql = "ORDER BY " . implode(", ", $sql);
+
+        return $sql;
+    }
+
+    public function limit(int $limit): self
+    {
+        $this->limit = max(0, $limit);
+        return $this;
+    }
+
+    public function offset(int $offset): self
+    {
+        $this->offset = max(0, $offset);
+        return $this;
+    }
+
+    public function getSQL(bool $stripConditionsParentheses = false): string
+    {
+        if (isset($this->sql)) {
+            return $this->sql;
+        }
+
+        $sqls = [];
+
+        $sqls[] = $this->getBaseSQL();
+        $sqls[] = $this->getClausesSQL($stripConditionsParentheses);
+
+        $this->sql = trim(implode(" ", $sqls));
+
+        return $this->sql;
+    }
+
+    protected function getBaseSQL(): string
+    {
+        if (empty($this->table)) {
+            throw new RuntimeException(
+                "The SELECT FROM table has not been defined!"
+            );
+        }
+
+        $sqls = ["SELECT"];
+        if ($this->distinct) {
+            $sqls[] = "DISTINCT";
+        }
+
+        $sqls[] = $this->getColumnsSQL();
+
+        $table_sql = $this->quoteIdentifier($this->table);
+        if (!empty($this->alias) && $alias_sql = $this->quoteAlias($this->alias)) {
+            $table_sql .= " {$alias_sql}";
+        }
+
+        $sqls[] = "FROM {$table_sql}";
+
+        return trim(implode(" ", $sqls));
+    }
+
+    private function getClausesSQL(bool $stripConditionsParentheses = false): string
+    {
+        $sqls = [];
+
+        $join_sql = $this->getJoinSQL($stripConditionsParentheses);
+        if (!$this->isEmptySql($join_sql)) {
+            $sqls[] = $join_sql;
+        }
+
+        $where_sql = $this->getWhereSQL($stripConditionsParentheses);
+        if (!$this->isEmptySql($where_sql)) {
+            $sqls[] = $where_sql;
+        }
+
+        $group_sql = $this->getGroupBySQL();
+        if (!$this->isEmptySql($group_sql)) {
+            $sqls[] = $group_sql;
+        }
+
+        $having_sql = $this->getHavingSQL($stripConditionsParentheses);
+        if (!$this->isEmptySql($having_sql)) {
+            $sqls[] = $having_sql;
+        }
+
+        $order_sql = $this->getOrderBySQL();
+        if (!$this->isEmptySql($order_sql)) {
+            $sqls[] = $order_sql;
+        }
+
+        if ($this->limit > 0) {
+            $limit = $this->createNamedParam($this->limit, PDO::PARAM_INT);
+            $sqls[] = "LIMIT {$limit}";
+        }
+        if ($this->offset > 0) {
+            $offset = $this->createNamedParam($this->offset, PDO::PARAM_INT);
+            $sqls[] = "OFFSET {$offset}";
+        }
+
+        return implode(" ", $sqls);
+    }
     public function __get(string $name)
     {
         if ('where' === $name) {
