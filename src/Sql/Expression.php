1957b3ce7ea05477767924047ae41e7d5736f015
pine3ree <pine3ree@gmail.com>
2020-05-01 05:18:46 +0200

    add function imports to Sql\Expression
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- src/Sql/Expression.php
+++ src/Sql/Expression.php
@@ -12,6 +12,15 @@ use JsonSerializable;
 use P3\Db\Sql\Predicate;
 use PDO;
 
+use function addcslashes;
+use function is_bool;
+use function is_int;
+use function is_null;
+use function is_string;
+use function str_replace;
+use function strpos;
+use function trim;
+
 /**
  * This abstract class represents a generic SQL Expression and is the ancestor
  * of all the other sql-related classes.
