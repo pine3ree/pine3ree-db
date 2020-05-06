89548bf3705e561418ff7150b35ea04682a85242
pine3ree <pine3ree@gmail.com>
2020-05-06 10:38:51 +0200

    add Oci::decorateSelectSQL()
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- src/Sql/Driver/Oci.php
+++ src/Sql/Driver/Oci.php
@@ -10,6 +10,9 @@ namespace P3\Db\Sql\Driver;
 
 use P3\Db\Sql\Driver;
 
+use P3\Db\Sql\Statement\Select;
+use P3\Db\Sql\Statement\Insert;
+
 /**
  * Oci sql-driver
  */
@@ -17,11 +20,31 @@ class Oci extends Driver
 {
     public function __construct()
     {
-        parent::__construct('`', '`', "'");
+        parent::__construct('"', '"', "'");
     }
 
     public function getLimitSQL(int $limit = null, int $offset = null): string
     {
         return '';
+    }
+
+    public function decorateSelectSQL(Select $select, string $sql): string
+    {
+        $limit  = $select->limit;
+        $offset = $select->offset;
+
+        if (isset($limit) && !isset($offset)) {
+            return "SELECT * FROM ({$sql}) WHERE ROWNUM <= {$limit}";
+        }
+
+        if (isset($offset) && $offset > 0) {
+            if (!isset($limit)) {
+                $limit = PHP_INT_MAX;
+            }
+            $limit_sql = "SELECT *, ROWNUM AS __rownum FROM ({$sql}) WHERE ROWNUM <= {$limit}";
+            return "SELECT * FROM ($limit_sql) WHERE __rownum > {$offset}";
+        }
+
+        return $sql;
     }
 }
