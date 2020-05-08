bb174fbd8d8fcbc1097f35a0cbba42461d4728fe
pine3ree <pine3ree@gmail.com>
2020-05-08 15:42:15 +0200

    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- src/Command.php
+++ src/Command.php
@@ -1,8 +1,7 @@
 <?php
 
 /**
- * @package     p3-db
- * @author      pine3ree https://github.com/pine3ree
+ * @package p3-db@author  pine3ree https://github.com/pine3ree
  */
 
 namespace P3\Db;
--- src/Sql.php
+++ src/Sql.php
@@ -1,9 +1,8 @@
 <?php
 
 /**
- * @package     p3-db
- * 
- * @author      pine3ree https://github.com/pine3ree
+ * @package p3-db
+ * @author  pine3ree https://github.com/pine3ree
  */
 
 namespace P3\Db;
