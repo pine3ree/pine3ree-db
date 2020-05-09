ada0e6129564788a4a506baa1e178753a66c925c
pine3ree <pine3ree@gmail.com>
2020-05-09 16:48:41 +0200

    Add DbFactory::buildDsnForSqlSrv()
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- src/Factory/DbFactory.php
+++ src/Factory/DbFactory.php
@@ -47,10 +47,12 @@ class DbFactory
             );
         }
 
-        $this->params['dsn'] = $config['dsn'] ?? $this->buildDSN($config);
+        $config = array_change_key_case($config, CASE_LOWER);
+
+        $this->params['dsn']      = $config['dsn'] ?? $this->buildDSN($config);
         $this->params['username'] = $config['username'] ?? $config['user'] ?? null;
         $this->params['password'] = $config['password'] ?? $config['passwd'] ?? $config['pass'] ?? null;
-        $this->params['options'] = $config['options'] ?? [];
+        $this->params['options']  = $config['options'] ?? [];
     }
 
     private function buildDSN(array $config): string
@@ -159,5 +161,88 @@ class DbFactory
         }
 
         return "oci:dbname={$dbname}";
+    }
+
+    private function buildDsnForSqlSrv(string $dbname, array $config): string
+    {
+        $dsn = [];
+
+        $server = $config['server'] ?? $config['host'] ?? $config['hostname'] ?? null;
+        if (!empty($server)) {
+            $port = $config['port'] ?? null;
+            if (is_int($port)) {
+                $server .= ",{$port}";
+            }
+            $dsn[] = "Server={$server}";
+        }
+
+        $dsn[] = "Database={$dbname}";
+
+        $app = $config['app'] ?? null;
+        if (!empty($app)) {
+            $dsn[] = "APP={$app}";
+        }
+
+        $encrypt = $config['encrypt'] ?? null;
+        if (isset($encrypt)) {
+            $encrypt = (int)$encrypt;
+            $dsn[] = "Encrypt={$encrypt}";
+        }
+
+        $failover_partner = $config['failover_partner'] ?? null;
+        if (!empty($failover_partner)) {
+            $dsn[] = "Failover_Partner={$failover_partner}";
+        }
+
+        $login_timeout = $config['login_timeout'] ?? $config['logintimeout'] ?? null;
+        if (is_int($login_timeout)) {
+            $dsn[] = "LoginTimeout={$login_timeout}";
+        }
+
+        $mars = $config['multiple_active_result_sets'] ?? $config['multipleactiveresultsets'] ?? $config['mars'] ?? null;
+        if (!empty($mars)) {
+            $dsn[] = "MultipleActiveResultSets={$mars}";
+        }
+
+        $quoted_id = $config['quoted_id'] ?? $config['quotedid'] ?? null;
+        if (isset($quoted_id)) {
+            $quoted_id = (int)filter_var($quoted_id, FILTER_VALIDATE_BOOLEAN);
+            $dsn[] = "QuotedId={$quoted_id}";
+        }
+
+        $pooling = $config['connection_pooling'] ?? $config['connectionpooling'] ?? null;
+        if (isset($pooling)) {
+            $pooling = (int)filter_var($pooling, FILTER_VALIDATE_BOOLEAN);
+            $dsn[] = "ConnectionPooling={$pooling}";
+        }
+
+        $trace_file = $config['trace_file'] ?? $config['tracefile'] ?? null;
+        if (isset($trace_file)) {
+            $dsn[] = "TraceFile={$trace_file}";
+        }
+
+        $trace_on = $config['trace_on'] ?? $config['traceon'] ?? null;
+        if (isset($trace_on)) {
+            $trace_on = (int)filter_var($trace_on, FILTER_VALIDATE_BOOLEAN);
+            $dsn[] = "TraceOn={$trace_on}";
+        }
+
+        $transaction_isolation = $config['transaction_isolation'] ?? $config['transactionisolation'] ?? null;
+        if (is_int($transaction_isolation)) {
+            $dsn[] = "TransactionIsolation={$transaction_isolation}";
+        }
+
+        $trust_srv_cert = $config['trust_server_certificate'] ?? $config['trustservercertificate'] ?? null;
+        if (isset($trust_srv_cert)) {
+            $trust_srv_cert = (int)filter_var($trust_srv_cert, FILTER_VALIDATE_BOOLEAN);
+            $dsn[] = "TrustServerCertificate={$trust_srv_cert}";
+        }
+
+        $wsid = $config['wsid'] ?? null;
+        if (!empty($wsid)) {
+            $dsn[] = "WSID={$wsid}";
+        }
+
+        return 'sqlsrv:' . implode(';', $dsn);
     }
 }
