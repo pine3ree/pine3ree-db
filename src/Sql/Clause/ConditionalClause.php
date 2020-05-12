d87a1bfc3e0810300db1522c281f2a58c8dc6de5
pine3ree <pine3ree@gmail.com>
2020-05-12 20:31:19 +0200

    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- src/Sql/Clause/ConditionalClause.php
+++ src/Sql/Clause/ConditionalClause.php
@@ -10,7 +10,7 @@ namespace P3\Db\Sql\Clause;
 use P3\Db\Sql;
 use P3\Db\Sql\Driver;
 use P3\Db\Sql\Element;
-use P3\Db\Sql\Predicate\Set as PredicateSet;
+use P3\Db\Sql\Predicate;
 
 use function ltrim;
 use function preg_replace;
@@ -33,17 +33,51 @@ abstract class ConditionalClause extends Element
     protected $__name;
 
     /**
-     * @var PredicateSet
+     * @var Predicate\Set
      */
     protected $conditions;
 
     /**
      * @param string $combined_by One of `AND`, `OR`, `&&`, `||`
-     * @param null|Predicate[]|self|Predicate|array|string $predicates
+     * @param null|Predicate[]|self|Predicate|Predicate\Set|array|string $predicates
      */
     public function __construct(string $combined_by = null, $predicates = null)
     {
-        $this->conditions = new PredicateSet($combined_by, $predicates);
+        $this->conditions = new Predicate\Set($combined_by, $predicates);
+    }
+
+    /**
+     * Add a predicate or a predicate-set to this clause conditions
+     *
+     * @see Predicate\Set::addPredicate()
+     *
+     * @param Predicate|string|array $predicate A Predicate|Predicate\Set instance
+     *      or a specs-array [identifier, operator, value] or [identifier => value]
+     * @throws InvalidArgumentException
+     * @return $this Provides fluent interface
+     */
+    public function addPredicate($predicate)
+    {
+        $this->conditions->addPredicate($predicate);
+    }
+
+    /**
+     * @see Predicate\Set::isEmpty()
+     * @return bool
+     */
+    public function isEmpty(): bool
+    {
+        return $this->conditions->isEmpty();
+    }
+
+    public function getParams(): array
+    {
+        return $this->conditions->getParams();
+    }
+
+    public function getParamsTypes(bool $return_pdo_const_names = false): array
+    {
+        return $this->conditions->getParamsTypes($return_pdo_const_names);
     }
 
     public function getSQL(Driver $driver = null): string
@@ -56,7 +90,6 @@ abstract class ConditionalClause extends Element
         if (Sql::isEmptySQL($predicates_sql)) {
             return $this->sql = '';
         }
-        $this->importParams($this->conditions);
 
         $this->sql = "{$this->getName()} {$predicates_sql}";
         return $this->sql;
@@ -69,12 +102,12 @@ abstract class ConditionalClause extends Element
     protected function getName(): string
     {
         // use the statically defined name if set
-        if (!empty(static::$name)) {
+        if (isset(static::$name)) {
             return static::$name;
         }
 
         // use the cached name value if set
-        if (!empty($this->__name)) {
+        if (isset($this->__name)) {
             return $this->__name;
         }
 
@@ -83,5 +116,73 @@ abstract class ConditionalClause extends Element
         $this->__name = strtoupper($name);
 
         return $this->__name;
+    }
+
+    public function __call(string $methodName, array $arguments)
+    {
+        switch ($methodName) {
+            case 'all':
+               return $this->conditions->all(...$arguments);
+
+            case 'any':
+                return $this->conditions->any(...$arguments);
+
+            case 'between':
+                return $this->conditions->between(...$arguments);
+
+            case 'equal':
+                return $this->conditions->equal(...$arguments);
+
+            case 'exists':
+                return $this->conditions->exists(...$arguments);
+
+            case 'expression':
+                return $this->conditions->expression(...$arguments);
+
+            case 'greaterThan':
+                return $this->conditions->greaterThan(...$arguments);
+
+            case 'greaterThanEqual':
+                return $this->conditions->greaterThanEqual(...$arguments);
+
+            case 'in':
+                return $this->conditions->in(...$arguments);
+
+            case 'isNotNull':
+                return $this->conditions->isNotNull(...$arguments);
+
+            case 'isNull':
+                return $this->conditions->isNull(...$arguments);
+
+            case 'lessThan':
+                return $this->conditions->lessThan(...$arguments);
+
+            case 'lessThanEqual':
+                return $this->conditions->lessThanEqual(...$arguments);
+
+            case 'like':
+                return $this->conditions->like(...$arguments);
+
+            case 'literal':
+                return $this->conditions->literal(...$arguments);
+
+            case 'notBetween':
+                return $this->conditions->notBetween(...$arguments);
+
+            case 'notEqual':
+                return $this->conditions->notEqual(...$arguments);
+
+            case 'notBetween':
+                return $this->conditions->notBetween(...$arguments);
+
+            case 'notExists':
+                return $this->conditions->notExists(...$arguments);
+
+            case 'notIn':
+                return $this->conditions->notIn(...$arguments);
+
+            case 'notLike':
+                return $this->conditions->notLike(...$arguments);
+        }
     }
 }
