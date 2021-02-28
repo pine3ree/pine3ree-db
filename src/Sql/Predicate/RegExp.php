e59a0f8b9c25c6d1ea915f99c0d654534e24b2a9
pine3ree <pine3ree@gmail.com>
2021-02-28 15:37:50 +0100

    use PDO::PARAM_STR for RegExp predicate and add pattern assertion
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- src/Sql/Predicate/RegExp.php
+++ src/Sql/Predicate/RegExp.php
@@ -10,6 +10,13 @@ namespace P3\Db\Sql\Predicate;
 use P3\Db\Sql\Driver;
 use P3\Db\Sql\Literal;
 use P3\Db\Sql\Predicate;
+use PDO;
+
+use function get_class;
+use function gettype;
+use function is_object;
+use function is_string;
+use function sprintf;
 
 /**
  * This class represents a sql RegExp ~ OR ~* condition
@@ -29,10 +36,21 @@ class RegExp extends Predicate
     public function __construct($identifier, string $regexp, bool $case_sensitive = false)
     {
         self::assertValidIdentifier($identifier);
+        self::assertValidRegExp($regexp);
 
         $this->identifier = $identifier;
         $this->regexp = $regexp;
         $this->case_sensitive = $case_sensitive;
+    }
+
+    protected static function assertValidRegExp($regexp)
+    {
+        if (!is_string($regexp) && ! $regexp instanceof Literal) {
+            throw new InvalidArgumentException(sprintf(
+                "A REGEXP pattern must be either a string or an Sql Literal expression instance, `%s` provided!",
+                is_object($pattern) ? get_class($pattern) : gettype($pattern)
+            ));
+        }
     }
 
     public function getSQL(Driver $driver = null): string
@@ -57,7 +75,7 @@ class RegExp extends Predicate
             $operator = "{$operator}*";
         }
 
-        $param = $this->createParam($this->regexp);
+        $param = $this->createParam($this->regexp, PDO::PARAM_STR);
 
         return $this->sql = "{$identifier} {$operator} {$param}";
     }
