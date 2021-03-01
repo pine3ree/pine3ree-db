1dd4f6c86bebededb399a1f17d0838ddd65d4f92
pine3ree <pine3ree@gmail.com>
2021-03-02 00:08:38 +0100

    add all combination for is/is-not predicates and derived classes
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- test/Sql/Predicate/IsTest.php
+++ test/Sql/Predicate/IsTest.php
@@ -10,7 +10,7 @@ namespace P3\DbTest\Sql\Predicate;
 
 use InvalidArgumentException;
 use P3\Db\Sql;
-use P3\Db\Sql\Predicate\Is;
+use P3\Db\Sql\Predicate;
 use PHPUnit\Framework\TestCase;
 
 class IsTest extends TestCase
@@ -25,97 +25,151 @@ class IsTest extends TestCase
 
     public function testIsNull()
     {
-        $predicate = new Is('id', null);
+        $predicate = new Predicate\Is('id', null);
         self::assertEquals('"id" IS NULL', $predicate->getSQL());
     }
 
     public function testIsNullWithString()
     {
-        $predicate = new Is('id', 'NULL');
+        $predicate = new Predicate\Is('id', 'NULL');
         self::assertEquals('"id" IS NULL', $predicate->getSQL());
     }
 
     public function testIsNullWithStringIsCaseInsensitive()
     {
-        $predicate = new Is('id', 'null');
+        $predicate = new Predicate\Is('id', 'null');
         self::assertEquals('"id" IS NULL', $predicate->getSQL());
     }
 
     public function testIsTrue()
     {
-        $predicate = new Is('id', true);
+        $predicate = new Predicate\Is('id', true);
         self::assertEquals('"id" IS TRUE', $predicate->getSQL());
     }
 
     public function testIsTrueWithString()
     {
-        $predicate = new Is('id', 'TRUE');
+        $predicate = new Predicate\Is('id', 'TRUE');
         self::assertEquals('"id" IS TRUE', $predicate->getSQL());
     }
 
     public function testIsTrueWithStringIsCaseInsensitive()
     {
-        $predicate = new Is('id', 'true');
+        $predicate = new Predicate\Is('id', 'true');
         self::assertEquals('"id" IS TRUE', $predicate->getSQL());
     }
 
     public function testIsFalse()
     {
-        $predicate = new Is('id', false);
+        $predicate = new Predicate\Is('id', false);
         self::assertEquals('"id" IS FALSE', $predicate->getSQL());
     }
 
     public function testIsFalseWithString()
     {
-        $predicate = new Is('id', 'FALSE');
+        $predicate = new Predicate\Is('id', 'FALSE');
         self::assertEquals('"id" IS FALSE', $predicate->getSQL());
     }
 
     public function testIsFalseWithStringIsCaseInsensitive()
     {
-        $predicate = new Is('id', 'false');
+        $predicate = new Predicate\Is('id', 'false');
         self::assertEquals('"id" IS FALSE', $predicate->getSQL());
     }
 
     public function testIsUnknown()
     {
-        $predicate = new Is('id', 'UNKNOWN');
+        $predicate = new Predicate\Is('id', 'UNKNOWN');
         self::assertEquals('"id" IS UNKNOWN', $predicate->getSQL());
     }
 
     public function testIsUnknownIsCaseInsensitive()
     {
-        $predicate = new Is('id', 'unKnowN');
+        $predicate = new Predicate\Is('id', 'unKnowN');
         self::assertEquals('"id" IS UNKNOWN', $predicate->getSQL());
     }
 
     public function testInvalidValueRaiseException()
     {
         $this->expectException(InvalidArgumentException::class);
-        $predicate = new Is('id', 'invalid-value');
+        $predicate = new Predicate\Is('id', 'invalid-value');
     }
 
     public function testWhenIdentifierIsLiteral()
     {
-        $predicate = new Is(Sql::literal('(1 = 1) AS OneEqualsOne'), null);
+        $predicate = new Predicate\Is(Sql::literal('(1 = 1) AS OneEqualsOne'), null);
         self::assertEquals('(1 = 1) AS OneEqualsOne IS NULL', $predicate->getSQL());
     }
 
     public function testWhenIdentifierIsAlias()
     {
-        $predicate = new Is(Sql::alias('customer.isEnabled'), true);
+        $predicate = new Predicate\Is(Sql::alias('customer.isEnabled'), true);
         self::assertEquals('"customer.isEnabled" IS TRUE', $predicate->getSQL());
 
-        $predicate = new Is(Sql::alias('some.alias'), null);
+        $predicate = new Predicate\Is(Sql::alias('some.alias'), null);
         self::assertEquals('"some.alias" IS NULL', $predicate->getSQL());
     }
 
     public function testWhenIdentifierHasTableOrAliasPrefix()
     {
-        $predicate = new Is('u.id', null);
+        $predicate = new Predicate\Is('u.id', null);
         self::assertEquals('"u"."id" IS NULL', $predicate->getSQL());
 
-        $predicate = new Is('user.id', null);
+        $predicate = new Predicate\Is('user.id', null);
         self::assertEquals('"user"."id" IS NULL', $predicate->getSQL());
+    }
+
+    public function testIsNot()
+    {
+        $predicate = new Predicate\IsNot('some_column', null);
+        self::assertEquals('"some_column" IS NOT NULL', $predicate->getSQL());
+
+        $predicate = new Predicate\IsNot('some_column', 'null');
+        self::assertEquals('"some_column" IS NOT NULL', $predicate->getSQL());
+
+        $predicate = new Predicate\IsNot('some_column', true);
+        self::assertEquals('"some_column" IS NOT TRUE', $predicate->getSQL());
+
+        $predicate = new Predicate\IsNot('some_column', 'true');
+        self::assertEquals('"some_column" IS NOT TRUE', $predicate->getSQL());
+
+        $predicate = new Predicate\IsNot('some_column', false);
+        self::assertEquals('"some_column" IS NOT FALSE', $predicate->getSQL());
+
+        $predicate = new Predicate\IsNot('some_column', 'false');
+        self::assertEquals('"some_column" IS NOT FALSE', $predicate->getSQL());
+
+        $predicate = new Predicate\IsNot('some_column', 'unknown');
+        self::assertEquals('"some_column" IS NOT UNKNOWN', $predicate->getSQL());
+    }
+
+    public function testIsFalsePredicate()
+    {
+        $predicate = new Predicate\IsFalse('some_column');
+        self::assertEquals('"some_column" IS FALSE', $predicate->getSQL());
+    }
+
+    public function testIsNotNullPredicate()
+    {
+        $predicate = new Predicate\IsNotNull('some_column');
+        self::assertEquals('"some_column" IS NOT NULL', $predicate->getSQL());
+    }
+
+    public function testIsNotUnknownPredicate()
+    {
+        $predicate = new Predicate\IsNotUnknown('some_column');
+        self::assertEquals('"some_column" IS NOT UNKNOWN', $predicate->getSQL());
+    }
+
+    public function testIsTruePredicate()
+    {
+        $predicate = new Predicate\IsTrue('some_column');
+        self::assertEquals('"some_column" IS TRUE', $predicate->getSQL());
+    }
+
+    public function testIsUnknownPredicate()
+    {
+        $predicate = new Predicate\IsUnknown('some_column');
+        self::assertEquals('"some_column" IS UNKNOWN', $predicate->getSQL());
     }
 }
