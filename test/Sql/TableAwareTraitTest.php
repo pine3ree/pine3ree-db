c9a610d3c570697b7f7817fb869cb9511479f674
pine3ree <pine3ree@gmail.com>
2021-03-07 02:19:39 +0100

    completed tests for TableAwareTrait
    Signed-off-by: pine3ree <pine3ree@gmail.com>

---
+++ test/Sql/TableAwareTraitTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @package     p3-db
+ * @subpackage  p3-db-test
+ * @author      pine3ree https://github.com/pine3ree
+ */
+
+namespace P3\DbTest\Sql;
+
+use InvalidArgumentException;
+use P3\Db\Sql\TableAwareTrait;
+use P3\DbTest\DiscloseTrait;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+class TableAwareTraitTest extends TestCase
+{
+    use DiscloseTrait;
+
+    public function setUp(): void
+    {
+    }
+
+    public function tearDown()
+    {
+    }
+
+    public function testSetEmptyTableRaisesException()
+    {
+        $tableAware = $this->getMockForTrait(TableAwareTrait::class);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->invokeMethod($tableAware, 'setTable', '  ');
+    }
+
+    public function testSetTableTwiceRaisesException()
+    {
+        $tableAware = $this->getMockForTrait(TableAwareTrait::class);
+
+        $this->invokeMethod($tableAware, 'setTable', 'cart_product');
+        $this->expectException(RuntimeException::class);
+        $this->invokeMethod($tableAware, 'setTable', 'store_product');
+    }
+}
