d18a48cddbb5fab7ac15df2ee2f3a4d580247879
pine3ree <pine3ree@gmail.com>
2021-03-07 22:29:55 +0100

    fix php-doc
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- test/DiscloseTrait.php
+++ test/DiscloseTrait.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * @package     package
- * @subpackage  package-subpackage
+ * @package     p3-db
+ * @subpackage  p3-db-test
  * @author      pine3ree https://github.com/pine3ree
  */
 
