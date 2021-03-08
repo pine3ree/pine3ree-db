2b4a32b7a2a1dbc308fed1bc03a1b15afe9c9026
pine3ree <pine3ree@gmail.com>
2021-03-08 18:12:24 +0100

    add custom exception classes
    Signed-off-by: pine3ree <pine3ree@gmail.com>

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package p3-db
+ * @author  pine3ree https://github.com/pine3ree
+ */
+
+namespace P3\Db\Exception;
+
+/**
+ * Class InvalidArgumentException
+ */
+class InvalidArgumentException extends \InvalidArgumentException
+{
+}
---
+++ src/Exception/RuntimeException.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package p3-db
+ * @author  pine3ree https://github.com/pine3ree
+ */
+
+namespace P3\Db\Exception;
+
+/**
+ * Class RuntimeException
+ */
+class RuntimeException extends \RuntimeException
+{
+}
