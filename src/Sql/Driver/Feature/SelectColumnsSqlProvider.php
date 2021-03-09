395f875f7825571ea482f10d35c0306a4e5111be
pine3ree <pine3ree@gmail.com>
2021-03-09 17:19:50 +0100

    add php-doc
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- src/Sql/Driver/Feature/SelectColumnsSqlProvider.php
+++ src/Sql/Driver/Feature/SelectColumnsSqlProvider.php
@@ -14,5 +14,12 @@ use P3\Db\Sql\Statement\Select;
  */
 interface SelectColumnsSqlProvider
 {
+    /**
+     * Customize SQL-string for a sql Select statement's columns
+     *
+     * @param Select $select
+     * @param bool $cache Flag tat will be set to true if generated sql can be cached
+     * @return string
+     */
     public function getSelectColumnsSQL(Select $select, bool &$cache = true): string;
 }
