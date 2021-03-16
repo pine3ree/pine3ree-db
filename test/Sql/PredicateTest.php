07d57cf5784db6486421aa64244f371c7c6d6e19
pine3ree <pine3ree@gmail.com>
2021-03-16 14:34:19 +0100

    update predicate test
    Signed-off-by: pine3ree <pine3ree@gmail.com>

--- test/Sql/PredicateTest.php
+++ test/Sql/PredicateTest.php
@@ -16,6 +16,7 @@ use P3\Db\Sql\Driver;
 use P3\Db\Sql\DriverInterface;
 use P3\Db\Sql\Identifier;
 use P3\Db\Sql\Literal;
+use P3\Db\Sql\Params;
 use P3\Db\Sql\Predicate;
 use PHPUnit\Framework\TestCase;
 use stdClass;
@@ -54,19 +55,20 @@ class PredicateTest extends TestCase
                 $this->value = $value;
             }
 
-            public function getSQL(DriverInterface $driver = null): string
+            public function getSQL(DriverInterface $driver = null, Params $params = null): string
             {
                 if (isset($this->sql)) {
                     return $this->sql;
                 }
 
                 $driver = $driver ?? Driver::ansi();
+                $params = $params ?? ($this->params = new Params());
 
                 $sqls = [];
 
                 $sqls[] = $this->getIdentifierSQL($this->identifier, $driver);
                 $sqls[] = $this->operator;
-                $sqls[] = $this->getValueSQL($this->value, null, 'value');
+                $sqls[] = $this->getValueSQL($params, $this->value, null, 'value');
 
                 return $this->sql = implode(" ", $sqls);
             }
@@ -105,26 +107,26 @@ class PredicateTest extends TestCase
     public function testThatStringIdentifiersAreQuotedAsIdentifiers()
     {
         $predicate = $this->createInstance('tb.id', '=', 42);
-        self::assertStringMatchesFormat('"tb"."id" = :value%x', $predicate->getSQL());
+        self::assertStringMatchesFormat('"tb"."id" = :value%d', $predicate->getSQL());
     }
 
     public function testThatLiteralIdentifiersAreSupportedAndNotQuoted()
     {
         $literal = 'tb.column';
         $predicate = $this->createInstance(new Literal($literal), '=', 42);
-        self::assertStringMatchesFormat("{$literal} = :value%x", $predicate->getSQL());
+        self::assertStringMatchesFormat("{$literal} = :value%d", $predicate->getSQL());
     }
 
     public function testThatSqlIdentifiersAreSupportedAndQuotedProperly()
     {
         $predicate = $this->createInstance(new Identifier('tb.column'), '=', 42);
-        self::assertStringMatchesFormat('"tb"."column" = :value%x', $predicate->getSQL());
+        self::assertStringMatchesFormat('"tb"."column" = :value%d', $predicate->getSQL());
     }
 
     public function testThatAliasIdentifiersAreSupportedAndQuotedProperly()
     {
         $predicate = $this->createInstance(new Alias('my.alias'), '=', 42);
-        self::assertStringMatchesFormat('"my.alias" = :value%x', $predicate->getSQL());
+        self::assertStringMatchesFormat('"my.alias" = :value%d', $predicate->getSQL());
     }
 
     /**
@@ -172,7 +174,7 @@ class PredicateTest extends TestCase
         $predicate = $this->createInstance('t.id', Sql::EQ, 42);
 
         self::assertStringMatchesFormat(
-            '"t"."id" = :value%x',
+            '"t"."id" = :value%d',
             $sql = $predicate->getSQL()
         );
 
@@ -180,10 +182,11 @@ class PredicateTest extends TestCase
         self::assertSame($sql, $predicate->getSQL());
 
         $params = $predicate->getParams();
-        $params_keys = array_keys($params);
-        $params_values = array_values($params);
+        $values = $params->getValues();
+        $params_keys = array_keys($values);
+        $params_values = array_values($values);
 
         self::assertSame([42], $params_values);
-        self::assertStringMatchesFormat(':value%x', $params_keys[0]);
+        self::assertStringMatchesFormat(':value%d', $params_keys[0]);
     }
 }
